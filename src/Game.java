1abbbc9b548aac500b9a6e214dfd5af0dfa2bdf9
logankinsella6 <77296134+logankinsella6@users.noreply.github.com>
2024-03-10 16:09:51 -0500

    v1.0

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
 // Written by Logan Kinsella, kinse124
 public class Game {
+    public static void main(String[] args){
+
+    }
 }
--- src/README.txt
+++ src/README.txt
@@ -1,4 +1,1 @@
 Written by Logan Kinsella, kinse124
-
-To-do:
-- Add more comments
